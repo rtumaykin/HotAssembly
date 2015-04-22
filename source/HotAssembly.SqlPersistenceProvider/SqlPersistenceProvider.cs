f3701c5db16b2b373d363cfca2e03bc5d68c391c
Roman Tumaykin <roman@tumaykin.com>
2015-04-22 00:10:08 -0700

    Basic layout

---
+++ source/HotAssembly.SqlPersistenceProvider/SqlPersistenceProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotAssembly
+{
+    public class SqlPersistenceProvider : IPersistenceProvider
+    {
+        public bool GetBundle(Guid bundleId, string localPath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool PersistBundle(Guid bundleId, string localPath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ source/HotAssembly/IPersistenceProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotAssembly
+{
+    public interface IPersistenceProvider
+    {
+        bool GetBundle(Guid bundleId, string localPath);
+        bool PersistBundle(Guid bundleId, string localPath);
+    }
+}
