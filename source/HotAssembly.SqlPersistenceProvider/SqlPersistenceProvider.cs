95d81b8bfd985b663da889e0148f79662c52862c
Roman Tumaykin <roman@tumaykin.com>
2015-10-09 23:37:08 -0700

    Added conditional filestream access based on whether the credentials are SQL or Windows. Filestream does not work with sql authentication

--- source/HotAssembly.SqlPersistenceProvider/SqlPersistenceProvider.cs
+++ source/HotAssembly.SqlPersistenceProvider/SqlPersistenceProvider.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.SqlClient;
 using System.Data.SqlTypes;
 using System.IO;
 using HotAssembly.PersistenceProviderRepository;
@@ -7,82 +8,121 @@ namespace HotAssembly
 {
     public class SqlPersistenceProvider : IPersistenceProvider
     {
+        private enum SqlAuthenticationType
+        {
+            Sql,
+            Windows
+        };
+
+        private SqlAuthenticationType AuthenticationType { get; }
+
+        public SqlPersistenceProvider(string connectionString = null)
+        {
+            if (string.IsNullOrWhiteSpace(connectionString))
+                return;
+
+            ExecutionScope.ConnectionString = connectionString;
+            AuthenticationType = new SqlConnectionStringBuilder(connectionString).IntegratedSecurity
+                ? SqlAuthenticationType.Windows
+                : SqlAuthenticationType.Sql;
+        }
+
         public void GetBundle(string bundleId, string destinationPath)
         {
-            using (var scope = new ExecutionScope())
+            switch (AuthenticationType)
             {
-                var result = PersistenceProviderRepository.Executables.common.GetBundleFileStreamData
-                    .Execute(
-                        bundleId,
-                        null, null, scope);
+                case SqlAuthenticationType.Sql:
+                    var getBundle = PersistenceProviderRepository.Executables.common.GetBundle.Execute(bundleId, null);
+                    File.WriteAllBytes(destinationPath, getBundle.Parameters.Bundle);
+                    break;
 
-                var inputPath = result.Parameters.BundlePath;
-
-                using (
-                    var sqlFileStream = new SqlFileStream(inputPath,
-                        result.Parameters.TransactionContext, FileAccess.Read, FileOptions.SequentialScan, 0))
-                {
-                    using (
-                        var localFileStream = new FileStream(destinationPath, FileMode.Create, FileAccess.Write)
-                        )
+                case SqlAuthenticationType.Windows:
+                    using (var scope = new ExecutionScope())
                     {
-                        const int bufferSize = 4096;
-                        var buffer = new byte[bufferSize];
+                        var getBundleFileStreamData = PersistenceProviderRepository.Executables.common.GetBundleFileStreamData
+                            .Execute(
+                                bundleId,
+                                null, null, scope);
 
-                        var bytesRead = sqlFileStream.Read(buffer, 0, bufferSize);
-                        while (bytesRead > 0)
+                        var inputPath = getBundleFileStreamData.Parameters.BundlePath;
+
+                        using (
+                            var sqlFileStream = new SqlFileStream(inputPath,
+                                getBundleFileStreamData.Parameters.TransactionContext, FileAccess.Read, FileOptions.SequentialScan, 0))
                         {
-                            localFileStream.Write(buffer, 0, bytesRead);
-                            localFileStream.Flush();
+                            using (
+                                var localFileStream = new FileStream(destinationPath, FileMode.Create, FileAccess.Write)
+                                )
+                            {
+                                const int bufferSize = 4096;
+                                var buffer = new byte[bufferSize];
 
-                            bytesRead = sqlFileStream.Read(buffer, 0, bufferSize);
+                                var bytesRead = sqlFileStream.Read(buffer, 0, bufferSize);
+                                while (bytesRead > 0)
+                                {
+                                    localFileStream.Write(buffer, 0, bytesRead);
+                                    localFileStream.Flush();
+
+                                    bytesRead = sqlFileStream.Read(buffer, 0, bufferSize);
+                                }
+
+                                localFileStream.Close();
+                            }
+
+                            sqlFileStream.Close();
                         }
-
-                        localFileStream.Close();
+                        scope.Commit();
                     }
-
-                    sqlFileStream.Close();
-                }
-                scope.Commit();
+                    break;
             }
         }
 
         public void PersistBundle(string bundleId, string sourcePath)
         {
-            using (var scope = new ExecutionScope())
+            switch (AuthenticationType)
             {
-                var result = PersistenceProviderRepository.Executables.common.PrepareSaveBundleFileStreamData
-                    .Execute(
-                        bundleId,
-                        null, null, scope);
+                case SqlAuthenticationType.Sql:
+                    var data = File.ReadAllBytes(sourcePath);
+                    PersistenceProviderRepository.Executables.common.SaveBundle.Execute(bundleId, data);
+                    break;
 
-                var outputPath = result.Parameters.BundlePath;
-
-                using (var localFileStream = new FileStream(sourcePath, FileMode.Open, FileAccess.Read))
-                {
-                    using (
-                        var sqlFileStream = new SqlFileStream(outputPath,
-                            result.Parameters.TransactionContext, FileAccess.Write, FileOptions.SequentialScan,
-                            0))
+                case SqlAuthenticationType.Windows:
+                    using (var scope = new ExecutionScope())
                     {
-                        const int bufferSize = 4096;
-                        var buffer = new byte[bufferSize];
+                        var result = PersistenceProviderRepository.Executables.common.PrepareSaveBundleFileStreamData
+                            .Execute(
+                                bundleId,
+                                null, null, scope);
 
-                        var bytesRead = localFileStream.Read(buffer, 0, bufferSize);
-                        while (bytesRead > 0)
+                        var outputPath = result.Parameters.BundlePath;
+
+                        using (var localFileStream = new FileStream(sourcePath, FileMode.Open, FileAccess.Read))
                         {
-                            sqlFileStream.Write(buffer, 0, bytesRead);
-                            sqlFileStream.Flush();
+                            using (
+                                var sqlFileStream = new SqlFileStream(outputPath,
+                                    result.Parameters.TransactionContext, FileAccess.Write, FileOptions.SequentialScan,
+                                    0))
+                            {
+                                const int bufferSize = 4096;
+                                var buffer = new byte[bufferSize];
 
-                            bytesRead = localFileStream.Read(buffer, 0, bufferSize);
+                                var bytesRead = localFileStream.Read(buffer, 0, bufferSize);
+                                while (bytesRead > 0)
+                                {
+                                    sqlFileStream.Write(buffer, 0, bytesRead);
+                                    sqlFileStream.Flush();
+
+                                    bytesRead = localFileStream.Read(buffer, 0, bufferSize);
+                                }
+
+                                sqlFileStream.Close();
+                            }
+                            localFileStream.Close();
                         }
 
-                        sqlFileStream.Close();
+                        scope.Commit();
                     }
-                    localFileStream.Close();
-                }
-
-                scope.Commit();
+                    break;
             }
         }
     }
