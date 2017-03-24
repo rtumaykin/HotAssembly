58701e1db773412bd1529826f33082845abf7edb
Roman Tumaykin <roman@tumaykin.com>
2017-03-24 16:43:09 -0700

    Bumped verions

--- source/HotAssembly/Properties/AssemblyInfo.cs
+++ source/HotAssembly/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Roman Tumaykin")]
 [assembly: AssemblyProduct("HotAssembly")]
-[assembly: AssemblyCopyright("Copyright © Roman Tumaykin 2015")]
+[assembly: AssemblyCopyright("Copyright © Roman Tumaykin 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.7.0")]
-[assembly: AssemblyFileVersion("0.9.7.0")]
+[assembly: AssemblyVersion("0.9.8.0")]
+[assembly: AssemblyFileVersion("0.9.8.0")]
